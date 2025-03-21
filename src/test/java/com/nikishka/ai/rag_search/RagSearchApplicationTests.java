39c849e601c62884d34ba30645bafc0c5db5be5a
nikishkaa <nikita.goutor@gmail.com>
2025-03-21 14:32:37 +0300

    Init.

---
+++ src/test/java/com/nikishka/ai/rag_search/RagSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikishka.ai.rag_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
